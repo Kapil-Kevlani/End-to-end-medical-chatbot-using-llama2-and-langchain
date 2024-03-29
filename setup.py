88b119d386e138dc39c49d9f3acdc208cb055ba7
kapil-kevlani <kapil-kevlani>
2024-03-29 16:10:33 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version = '0.0.0',
+    author = 'Kapil Kevlani',
+    author_email='kevlanikapil4@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
